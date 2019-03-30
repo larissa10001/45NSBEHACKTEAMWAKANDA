2eabb50459068d4f6e18238c7fedb75abeb6dca0
Toneke Graham <tonekegraham20@gmail.com>
2019-03-30 06:18:59 -0700

    almost finished site

---
+++ styling.js
@@ -0,0 +1,41 @@
+//smooth page transitions
+$(function(){
+  'use strict';
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 5,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
+
+  $('#main').smoothState({
+  onReady: {
+    duration: 250,
+    // `$container` is a `jQuery Object` of the the current smoothState container
+    // `$newContent` is a `jQuery Object` of the HTML that should replace the existing container's HTML.
+    render: function ($container, $newContent) {
+      // Update the HTML on the page
+      $container.html($newContent);
+    }
+  }
+});
